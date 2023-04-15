f8d1f0e8fb08ff9fcaa4f737e34e16d58675013a
OlegStankevich1973 <stankeviz@mail.ru>
2023-04-15 22:06:40 +0300

    Алгоритм обхода дерева в глубину

---
+++ RedBlackTreeLesson4/Tree.java
@@ -0,0 +1,41 @@
+package RedBlackTreeLesson4;
+   
+import java.util.List;
+import org.w3c.dom.Node;
+
+public class Tree {
+    Node root; // Базовое поле откуда дерево начинает свой отсчёт
+    // Классический обход дерева в глубину
+
+    public boolean exist(int value) {
+        if (root != null) {
+            Node node = find(root, value);
+            if (node != null) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    private Node find(Node node, int value) {
+        if (node.value == value) {
+            return node;
+        } else {
+            for (Node child : node.children) {
+                Node result = find(child, value);
+                if (result != null) {
+                    return result;
+                }
+            }
+        }
+        return null;
+    }
+
+    public class Node {
+        int value;
+        List<Node> children; // список детей
+    }
+}
+
+
